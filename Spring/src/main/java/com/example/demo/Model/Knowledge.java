58554a506d0ce043392f400479ddbd54570e2b05
yuezero <wuyl1256@gmail.com>
2025-06-11 00:57:07 +0800

    create Knowledge Model

---
+++ Spring/src/main/java/com/example/demo/Model/Knowledge.java
@@ -0,0 +1,54 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+
+@Table(name = "knowledges") // records表
+@Entity
+public class Knowledge {
+
+    @Column(name = "knowledge_id", nullable = false)
+    private String knowledgeId;
+
+    @Column(name = "knowledge_point", columnDefinition = "TEXT", nullable = false)
+    private String knowledgePoint; // 知識內容
+
+    @Column(name = "tags", columnDefinition = "TEXT[]", nullable = false)
+    private String[] tags; // 知識標籤
+
+    @Column(name = "summary", columnDefinition = "TEXT", nullable = false)
+    private String summary; // 知識內容摘要
+
+    @Column(name = "source", columnDefinition = "TEXT", nullable = false)
+    private String source; // 來源網址
+
+    public Knowledge() {
+    }
+
+    public Knowledge(String knowledgeId, String knowledgePoint, String[] tags, String summary, String source) {
+        this.knowledgeId = knowledgeId;
+        this.knowledgePoint = knowledgePoint;
+        this.tags = tags;
+        this.summary = summary;
+        this.source = source;
+    }
+
+    public String getKnowledgeId() {
+        return knowledgeId;
+    }
+
+    public String getKnowledgePoint() {
+        return knowledgePoint;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
