1e0ddfac8330950f116baec2bb32a4d6d1431331
yuezero <wuyl1256@gmail.com>
2025-06-15 00:46:56 +0800

    add record data

--- Spring/src/main/java/com/example/demo/dto/LoginRequest.java
+++ Spring/src/main/java/com/example/demo/dto/LoginRequest.java
@@ -13,6 +13,6 @@ public class LoginRequest {
     private String email;
 
     @NotBlank(message = "Password cannot be empty")
-    @Size(min = 6, max = 50, message = "Password must be between 8 and 50 characters")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
     private String password;
 }
