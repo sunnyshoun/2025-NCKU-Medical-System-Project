5d40de756e87fa5ed07c2e8a4aa354e918926b87
kurotail <billzi3huang@gmail.com>
2025-06-22 22:33:57 +0800

    Rename

---
+++ App/eye_dwell/lib/views/home.dart
@@ -0,0 +1,115 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:eye_dwell/controllers/cache_manager.dart';
+import 'package:eye_dwell/controllers/chat_controller.dart';
+import 'package:eye_dwell/controllers/settings_controller.dart';
+import 'package:eye_dwell/models/state_models.dart';
+import 'package:eye_dwell/views/chat_page.dart';
+import 'package:eye_dwell/views/remote/remote_page.dart';
+import 'package:eye_dwell/views/settings.dart';
+import 'package:eye_dwell/views/records_page.dart';
+import 'package:eye_dwell/configs/app_localizations.dart';
+
+class HomePage extends StatefulWidget {
+  final GeneralStateModel generalStates;
+  final HomeStateModel homeStates;
+
+  const HomePage({
+    super.key,
+    required this.generalStates,
+    required this.homeStates,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    log('init homepage');
+
+    final profile = widget.generalStates.profile;
+    log('profile: ${profile?.toJson().toString()}');
+
+    if (profile == null) {
+      log('profile is null, redirect to registration page');
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.of(context).pushReplacementNamed('/login');
+      });
+      return;
+    }
+
+    log('generalStates: ${widget.generalStates.toJson().toString()}');
+    CacheManager.savePreference(widget.generalStates);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final generalStates = widget.generalStates;
+    final homeStates = widget.homeStates;
+    final t = AppLocalizations(generalStates.locale);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: homeStates.selectedIndex,
+        children: [
+          VisionRecordsWidget(
+            generalStates: generalStates,
+            recordsStates: homeStates,
+          ),
+          RemotePage(states: generalStates),
+          SettingsPage(
+            controller: SettingsController(
+              states: generalStates,
+              setFontSizeChanged:
+                  (val) => setState(() => generalStates.fontSize = val),
+              setLocaleChanged:
+                  (val) => setState(() => generalStates.locale = val ?? 'zh'),
+              context: context,
+            ),
+            states: generalStates,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: homeStates.selectedIndex,
+        onTap: (index) {
+          setState(() => homeStates.selectedIndex = index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.visibility),
+            label: t.get('record'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.control_camera_outlined),
+            label: t.get('remote'),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings),
+            label: t.get('settings'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder:
+                  (_) => ChatPage(
+                    controller: ChatController(
+                      generalStates: generalStates,
+                      chatStates: ChatStateModel(),
+                    ),
+                  ),
+            ),
+          );
+        },
+        child: const Icon(Icons.chat),
+      ),
+    );
+  }
+}
---
+++ App/eye_dwell/lib/views/remote/remote_page.dart
@@ -0,0 +1,62 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:eye_dwell/models/state_models.dart';
+import 'package:eye_dwell/networks/blue.dart';
+import 'package:eye_dwell/views/remote/bluetooth_page.dart';
+import 'package:eye_dwell/views/remote/control_page.dart';
+import 'package:eye_dwell/views/remote/tester_menu.dart';
+
+class RemotePage extends StatefulWidget {
+  final GeneralStateModel states;
+  const RemotePage({super.key, required this.states});
+
+  @override
+  State<RemotePage> createState() => _RemotePageState();
+}
+
+class _RemotePageState extends State<RemotePage> {
+  late bool _started;
+  late TestMenuModel menuStates;
+
+  @override
+  void initState() {
+    super.initState();
+    _started = false;
+    menuStates = TestMenuModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    log('build RemotePage');
+    final states = widget.states;
+
+    if (states.blue == null) {
+      return BluetoothPage(
+        generalStates: states,
+        blueStates: BlueListStateModel(),
+        onConnected: () => setState(() {}),
+      );
+    }
+
+    return _started
+        ? ControlPage(
+          states: states,
+          menuStates: menuStates,
+          disconnect:
+              () => setState(() {
+                BLEInterface.sendCommand(states.blue!, {"type": "disconnect"});
+                states.blue = null;
+                _started = false;
+              }),
+        )
+        : TesterMenuPage(
+          generalStates: states,
+          menuStates: menuStates,
+          start:
+              () => setState(() {
+                _started = true;
+                BLEInterface.sendCommand(states.blue!, {'type': 'start_test'});
+              }),
+        );
+  }
+}
